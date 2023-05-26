9cf9a7ab5a6fad55fc6fca9857cc8c08d0785fca
mp <maulikk95@gmail.com>
2023-05-26 11:27:55 -0400

    created a post route to handle user input and send back modified data

--- server/server.js
+++ server/server.js
@@ -1,6 +1,39 @@
+const path = require('path');
 const express = require('express');
 const app = express()
 const port = process.env.port || 3001;
+
+app.use(express.json()); // Parse JSON data 
+
+let storedData = null;
+
+const DELIVERY_DATES = [
+  {
+    postal: "V",
+    ids: [2],
+    estimatedDeliveryDate: "Nov 24, 2021"
+  },
+  {
+    postal: "V",
+    ids: [1, 3],
+    estimatedDeliveryDate: "Nov 19, 2021"
+  },
+  {
+    postal: "M",
+    ids: [2, 3],
+    estimatedDeliveryDate: "Nov 22, 2021"
+  },
+  {
+    postal: "M",
+    ids: [1],
+    estimatedDeliveryDate: "Dec 19, 2021"
+  },
+  {
+    postal: "K",
+    ids: [1, 2, 3],
+    estimatedDeliveryDate: "Dec 24, 2021"
+  }
+];
 
 //Dummy Data
 const lineItems = [
@@ -30,8 +63,33 @@ const lineItems = [
     image:"https://www.cozey.ca/_next/image?url=https%3A%2F%2Fcdn.shopify.com%2Fs%2Ffiles%2F1%2F0277%2F3057%2F5462%2Fproducts%2F2_Single_shot_IVORY_OFF_OFF_SLOPE_5379af1f-9318-4e37-b514-962d33d1ce64.png%3Fv%3D1629231450&w=1920&q=75",
     swatchColor: "#F8F1EC",
     swatchTitle: "White"
-  }
+  },
 ];
+
+app.post("/api", (req, res) => {
+  const { postal, ids } = req.body;
+
+  // map through data to match the postal code
+  DELIVERY_DATES.forEach((item) => {
+    if(item.postal === postal){
+      storedData = item.estimatedDeliveryDate;
+    } else {
+      console.log('not matched');
+    }
+  })
+
+  // map through lineItems Data to update the estimatedDeliveryDate
+  const updatedLineItems = lineItems.map(item => {
+    return {
+      ...item,
+      estimatedDeliveryDate: storedData
+    };
+  });
+
+  // send the updated data to the client
+  res.json({ lineItems: updatedLineItems })
+
+});
 
 app.get("/api", (req, res) => {
   res.json({ lineItems:lineItems })
